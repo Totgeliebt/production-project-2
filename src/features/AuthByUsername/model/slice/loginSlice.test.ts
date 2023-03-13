f802d376151bec57604b89fa1e3873e9c21b7a2d
Natalia Zhuravskaia <zlaska@yandex.ru>
2023-03-13 18:55:42 +0300

    add tests for login module

---
+++ src/features/AuthByUsername/model/slice/loginSlice.test.ts
@@ -0,0 +1,16 @@
+import { DeepPartial } from '@reduxjs/toolkit';
+import { LoginSchema } from 'features/AuthByUsername';
+import { loginActions, loginReducer } from './loginSlice';
+
+describe('loginSlice.test', () => {
+    test('test set username', () => {
+        const state: DeepPartial<LoginSchema> = { username: 'admin' };
+        expect(loginReducer(state as LoginSchema, loginActions.setUsername('admina')))
+            .toEqual({ username: 'admina' });
+    });
+    test('test set password', () => {
+        const state: DeepPartial<LoginSchema> = { password: '123' };
+        expect(loginReducer(state as LoginSchema, loginActions.setPassword('12345')))
+            .toEqual({ password: '12345' });
+    });
+});
